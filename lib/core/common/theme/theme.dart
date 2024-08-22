683e8a7178b4c4b5c577efba39594cc5750f982b
sebastianvargas <sebastian98*>
2024-08-22 10:30:56 -0500

    Color theme added

--- lib/core/common/theme/theme.dart
+++ lib/core/common/theme/theme.dart
@@ -4,8 +4,22 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final appThemeMode = ThemeData().copyWith(
     scaffoldBackgroundColor: AppColors.backgroundColor,
+    primaryColor: Colors.blue,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.gradient2,
+    ),
     appBarTheme: const AppBarTheme(
       backgroundColor: AppColors.backgroundColor,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.blue,
+      ),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: AppColors.backgroundColor,
+      selectedItemColor: AppColors.gradient2,
+      unselectedItemColor: AppColors.greyColor,
     ),
     chipTheme: const ChipThemeData(
       color: WidgetStatePropertyAll(
